9084d58cb23f2fc11a6db0cb041abdaff52bcabc
Pramit Dutta <pramitduttaanik@gmail.com>
2021-08-10 00:35:59 +0600

    Initial Commit

---
+++ src/Login.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import './Login.css'
+import swal from 'sweetalert'
+function Login() {
+    const [username, setusername] = useState('')
+    const [password, setpassword] = useState('')
+    function validate(event) {
+        event.preventDefault();
+        if (username == 'python' && password == 'tensorflow') {
+            swal('Login Successful', 'Congratulation your login is successful', 'success')
+        } else {
+            swal('Invalid Login Details', 'Please check your details once more', 'error')
+        }
+    }
+    return (
+        <div>
+            <div class="row justify-content-center">
+                <div class="col-md-4">
+                    <h1>User Authentication</h1>
+                    <form onSubmit={validate}>
+                        <input type="text" placeholder="username" className='form-control' value={username}
+                            onChange={(e) => { setusername(e.target.value) }} />
+                        <input type="password" placeholder="password" className='form-control' value={password}
+                            onChange={(e) => { setpassword(e.target.value) }} />
+                        <input type="submit" className="btn btn-primary" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Login;
